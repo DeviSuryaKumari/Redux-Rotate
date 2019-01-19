4ed82d0949b75fdfd0fc57ef150d29df25efa90d
Devi Akula <devi.akula@hexagon.com>
2019-01-19 20:36:54 +0530

    React logo rotation controlled by Redux state management

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from "react-redux";
+import { startAction } from "./actions/startAction";
+import { stopAction } from "./actions/stopAction";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className={"App-logo" + (this.props.rotating ? "":" App-logo-paused")} 
+          alt="logo" onClick={ this.props.rotating ? this.props.stopAction : this.props.startAction }/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -25,4 +29,13 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+});
+const mapDispatchToProps = dispatch => ({
+  startAction: () => dispatch(startAction),
+  stopAction: () => dispatch(stopAction)
+});
+
+//export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import rotateReducer from "./reducers/rotateReducer";
+import { createStore } from "redux";
+
+function configureStore(state = { rotating: true }) {
+  return createStore(rotateReducer,state);
+}
+export default configureStore;
